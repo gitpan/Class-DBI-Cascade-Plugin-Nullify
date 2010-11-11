b53c70e21af0d39a0121607cf58c53c9c5aed2cd
Xufeng Liang <danny.glue@gmail.com>
2010-11-11 23:13:34 +0000

    Import of DANNY/Class-DBI-Cascade-Plugin-Nullify-0.05 from CPAN.
    gitpan-cpan-distribution: Class-DBI-Cascade-Plugin-Nullify
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         DANNY/Class-DBI-Cascade-Plugin-Nullify-0.05.tar.gz
    gitpan-cpan-author:       DANNY
    gitpan-cpan-maturity:     released

--- lib/Class/DBI/Cascade/Plugin/Nullify.pm
+++ lib/Class/DBI/Cascade/Plugin/Nullify.pm
@@ -1,5 +1,27 @@
 package Class::DBI::Cascade::Plugin::Nullify;
 
+use strict;
+use warnings;
+
+use base 'Class::DBI::Cascade::None';
+
+our $VERSION = 0.05;
+
+sub cascade {
+	my ($self, $obj) = @_;
+	my $foreign_objects = $self->foreign_for($obj); # get all foreign objects
+	my $foreign_key = $self->{_rel}->args->{foreign_key}; # get the foreign key
+	
+	while ( my $foreign_object = $foreign_objects->next) {	
+		$foreign_object->$foreign_key(undef); # set foreign key value to null
+		$foreign_object->update(); # update the object
+	}
+}
+
+1;
+
+__END__
+
 =head1 NAME
 
 Class::DBI::Cascade::Plugin::Nullify - Nullify related Class::DBI objects
@@ -16,38 +38,11 @@ This is a cascading nullify strategy (i.e. 'on delete set null') that will nulli
 
 THIS MODULE IS NOT LONGER DEVELOPED. Please consider L<Rose::DB::Object> as a alternative to L<Class::DBI>.
 
-=cut
-
-use strict;
-use warnings;
-
-use base 'Class::DBI::Cascade::None';
-
-our $VERSION = 0.04;
-
-
 =head1 METHODS
 
-=over 4
+=head2 C<cascade>
 
-=item cascade
-
-implementation of the cascading nullify strategy. 
-
-=back
-
-=cut
-
-sub cascade {
-	my ($self, $obj) = @_;
-	my $foreign_objects = $self->foreign_for($obj); # get all foreign objects
-	my $foreign_key = $self->{_rel}->args->{foreign_key}; # get the foreign key
-	
-	while ( my $foreign_object = $foreign_objects->next) {	
-		$foreign_object->$foreign_key(undef); # set foreign key value to null
-		$foreign_object->update(); # update the object
-	}
-}
+implementation of the cascading nullify strategy.
 
 =head1 AUTHOR
 
@@ -60,5 +55,3 @@ Copyright 2006-2010 Xufeng (Danny) Liang, All Rights Reserved.
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
-
-1;
